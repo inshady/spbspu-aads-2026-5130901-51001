a1ba1ec6bfa2910635bf18e9434e5798c9bd912b
inshady <inshadyprod@gmail.com>
2026-05-26 11:51:10 +0300

    add impl

--- hachaturyanov.ivan/S3/hash_table.hpp
+++ hachaturyanov.ivan/S3/hash_table.hpp
@@ -5,6 +5,41 @@
 
 namespace hachaturyanov
 {
+  static constexpr size_t DEFAULT_CAPACITY = 17;
+
+  inline bool isPrime(size_t n) {
+    if (n <= 1) {
+      return false;
+    }
+    if (n <= 3) {
+      return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+      return false;
+    }
+    for (size_t i = 5; i * i <= n; i += 6) {
+      if (n % i == 0 || n % (i + 2) == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  inline size_t nextPrime(size_t n) {
+    if (n <= 2) {
+      return 2;
+    }
+    if (n % 2 == 0) {
+      n++;
+    } else {
+      n += 2;
+    }
+    while (!isPrime(n)) {
+      n += 2;
+    }
+    return n;
+  }
+
   enum State {
     EMPTY,
     OCCUPIED,
@@ -28,7 +63,7 @@ namespace hachaturyanov
     bool has(const Key &key) const;
     void rehash(size_t slots);
    private:
-    List< Slot< Key, Value > > *data_;
+    Slot< Key, Value >* data_;
     size_t size_;
     size_t capacity_;
     Hash hash_;
@@ -37,17 +72,49 @@ namespace hachaturyanov
 
   template< class Key, class Value, class Hash, class Equal >
   HashTable< Key, Value, Hash, Equal >::HashTable():
-   data_(nullptr),
+   data_(new Slot< Key, Value >[DEFAULT_CAPACITY]),
    size_(0),
-   capacity_(0),
+   capacity_(DEFAULT_CAPACITY)
   {}
 
   template< class Key, class Value, class Hash, class Equal >
   HashTable< Key, Value, Hash, Equal >::HashTable(size_t slots):
-   data_(new List< Slot< Key, Value > >[slots]),
+   data_(new Slot< Key, Value >[slots]),
    size_(0),
-   capacity_(slots)
+   capacity_(nextPrime(slots))
   {}
 
+  template< class Key, class Value, class Hash, class Equal >
+  void HashTable< Key, Value, Hash, Equal >::add(const Key &key, const Value &value)
+  {
+    bool added = false;
+    bool tombstone_found = false;
+    size_t tombstone_id = 0;
+    for (size_t i = 0; i < capacity_; i++) {
+      size_t id = (hash_(key) + i * i) % capacity_;
+      Slot< Key, Value > &slot = data_[id];
+      if (slot.state == EMPTY) {
+        if (tombstone_found) {
+          slot = data_[tombstone_id];
+        }
+        slot.key = key;
+        slot.value = value;
+        slot.state = OCCUPIED;
+        size_++;
+        added = true;
+        break;
+      } else if (slot.state == TOMBSTONE && !tombstone_found) {
+        tombstone_found = true;
+        tombstone_id = id;
+      } else if (equal_(slot.key, key)) {
+        slot.value = value;
+        break;
+      }
+    }
+
+    if (!added) {
+      throw std::runtime_error("Failed to add key-value pair, rehashing recommended");
+    }
+  }
 }
 #endif
