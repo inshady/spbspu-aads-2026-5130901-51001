86b3730fbacb4634121d960484e736891aec4e07
inshady <inshadyprod@gmail.com>
2026-06-11 19:06:58 +0300

    insertSorted and find

--- hachaturyanov.ivan/common/list.hpp
+++ hachaturyanov.ivan/common/list.hpp
@@ -39,6 +39,10 @@ namespace hachaturyanov
     iter insertBefore(iter pos, T &&val);
     iter insertAfter(iter pos, T &&val);
 
+    iter insertSorted(const T &val);
+
+    iter find(const T &val);
+
     void popFront() noexcept;
     void popEnd() noexcept;
 
@@ -50,6 +54,33 @@ namespace hachaturyanov
     List& operator=(const List &other);
     List& operator=(List &&other);
   };
+
+  template< class T > typename List< T >::iter List< T >::insertSorted(const T &val)
+  {
+    if (isEmpty()) {
+      return addEnd(val);
+    }
+    auto it = begin();
+    do {
+      ++it;
+    } while (it != begin() && *it < val);
+    return insertBefore(it, val);
+  }
+
+  template< class T > typename List< T >::iter List< T >::find(const T &val)
+  {
+    if (!isEmpty()) {
+      iter first = begin();
+      iter it = first;
+      do {
+        if (*it == val) {
+          return it;
+        }
+        ++it;
+      } while (it != first);
+    }
+    return end();
+  }
 
   template< class T > bool List< T >::has(const T &val) const noexcept
   {
